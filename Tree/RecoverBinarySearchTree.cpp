ef1293463fb0fafbbddb64b816a8862b2851ce2f
apurvawani <apurva.wani06@gmail.com>
2022-07-28 00:01:30 +0530

    Google Interview Prep

---
+++ Tree/RecoverBinarySearchTree.cpp
@@ -0,0 +1,54 @@
+/*
+You are given the root of a binary search tree (BST), where the values of exactly two nodes of the tree were swapped by mistake. Recover the tree without changing its structure.
+
+
+Example 1:
+Input: root = [1,3,null,null,2]
+Output: [3,1,null,null,2]
+Explanation: 3 cannot be a left child of 1 because 3 > 1. Swapping 1 and 3 makes the BST valid.
+
+Example 2:
+Input: root = [3,1,4,null,null,2]
+Output: [2,1,4,null,null,3]
+Explanation: 2 cannot be in the right subtree of 3 because 2 < 3. Swapping 2 and 3 makes the BST valid.
+ 
+
+Constraints:
+The number of nodes in the tree is in the range [2, 1000].
+-2^31 <= Node.val <= 2^31 - 1
+*/
+
+class Solution {
+public:
+    TreeNode *prev;
+    TreeNode *first; 
+    TreeNode *middle;
+    TreeNode *last;
+  
+    void inorder(TreeNode* root) {
+        if(root->left)
+            inorder(root->left);
+        if(prev && (prev->val > root->val)) {
+            if(first != NULL)
+                last = root;
+            else {
+                first = prev;
+                middle = root;   
+            }
+        }
+        prev = root;
+        if(root->right)
+            inorder(root->right);
+    }
+  
+    void recoverTree(TreeNode* root) {
+        if(root == NULL)
+            return;
+        prev = first = middle = last = NULL;
+        inorder(root);
+        if(first && last)
+            swap(first->val, last->val);
+        else if(first && middle)
+            swap(first->val, middle->val);
+    }
+};
